74c681cf49f0fd889e0fe0ca1ca934d9948fb1ff
Samantha Bothwell <46718943+samantha-bothwell@users.noreply.github.com>
2026-03-24 11:48:09 -0600

    Add activity summary to table1

--- Rscripts/data-analysis/01_demographics.R
+++ Rscripts/data-analysis/01_demographics.R
@@ -16,13 +16,16 @@ library(readr)
 library(table1)
 library(Hmisc)
 
+
 ## Source functions
 source(here::here("Rfunctions", "table1_helpers.R"))
+
 
 ## Load demographic data 
 dems <- read_csv(here::here("data-clean", "identifiable", "Demographics.csv")) %>% 
   ## Keep only complete data 
   filter(!is.na(calc_age))
+
 
 ## Assign labels to dems variables 
 label(dems$calc_age) <- "Age (Years)"
@@ -30,10 +33,14 @@ label(dems$nih_ethnicity_race) <- "Race/Ethnicity"
 label(dems$pe_wt) <- "Weight (kg)"
 label(dems$pe_ht) <- "Height (cm)"
 label(dems$pe_bmi) <- "BMI (kg/m^2)"
+label(dems$season) <- "Activity School-Year vs Season Collection"
+label(dems$n_days) <- "Number of Usable Activity Monitor Days (>70% Wear Time)"
+label(dems$percent_wear) <- "Percent Wear Time Across Usable Days"
 
 
 ## Print table1
-table1(~ calc_age + nih_ethnicity_race + pe_wt + pe_ht + pe_bmi | group, 
+table1(~ calc_age + nih_ethnicity_race + pe_wt + pe_ht + pe_bmi + season + 
+         n_days + percent_wear | group, 
        data = dems, overall = F, extra.col=list(`P-value`= pvalue), 
        render.continuous = my.render.cont)
 
