1b5036e135c37619b9faf9ba60720d4c28257f5a
Samantha Bothwell <46718943+samantha-bothwell@users.noreply.github.com>
2026-06-08 13:47:52 -0600

    time period for sleep stages

--- Rscripts/data-analysis/06_sleep_mixture_model.R
+++ Rscripts/data-analysis/06_sleep_mixture_model.R
@@ -33,27 +33,29 @@ sleep$state_mix <- factor(mix_fit$classification,
 
 # Get sleep stage summary per person and night
 sleep_sum <- sleep %>% 
-  group_by(ID, night, state_mix, group) %>% 
+  mutate(time_period = ifelse(weekday == "Weekend" | school == "Summer", 
+                              "Weekend or Summer Night", "School-Night")) %>% 
+  group_by(ID, night, state_mix, group, time_period) %>% 
   summarise(N = n()) %>% 
   ungroup() %>% 
-  group_by(ID, night) %>% 
+  group_by(ID, night, time_period) %>% 
   mutate(prop = N/sum(N)) %>% 
   ungroup()
 
 
-sleep_stages <- ggplot(sleep_sum, aes(x = state_mix, y = prop, fill = group)) + 
+sleep_stages <- ggplot(sleep_sum, aes(x = group, y = prop, fill = group)) + 
   geom_boxplot() + 
   theme_bw(base_size = 16) +
   theme(legend.position = "bottom", 
         axis.text.x = element_blank(), 
         axis.ticks.x = element_blank()) +
   stat_compare_means(method = "wilcox.test", label = "p.signif", label.y.npc = "top", 
-                     vjust = 0.7, label.x.npc = 0.4, hide.ns = T, size = 6) +
+                     vjust = 0.7, label.x.npc = 0.5, hide.ns = T, size = 6) +
   scale_fill_manual(values = c("#369dd9", "#6D6D6D")) + 
-  facet_wrap(~state_mix, scales = "free") + 
+  facet_grid(state_mix~time_period, scales = "free_y") + 
   labs(x = "", y = "Proportion of Sleeping Time", fill = "")
 
-ggsave(filename = here::here("outputs", "sleep_stages.png"), plot = sleep_stages, width = 7, height = 7, units = "in")
+ggsave(filename = here::here("outputs", "sleep_stages.png"), plot = sleep_stages, width = 6, height = 9, units = "in")
 
 
 
